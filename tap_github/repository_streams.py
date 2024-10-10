a120f8810aebf1a30cf3f0b5ad2b3a34ed028b54
Trish Gillett-Kawamoto <trish.gillett@shopify.com>
2024-10-10 10:45:03 -0600

    fix schema and add a couple fields

--- tap_github/repository_streams.py
+++ tap_github/repository_streams.py
@@ -1848,8 +1848,10 @@ class DiscussionsStream(GitHubGraphqlStream):
                   }
                   author_association: authorAssociation
                   category {
-                    id
+                    node_id: id
+                    slug
                     name
+                    description
                   }
                   updated_at: updatedAt
                   created_at: createdAt
@@ -1891,8 +1893,10 @@ class DiscussionsStream(GitHubGraphqlStream):
         """  # noqa: E501
 
     category_object = th.ObjectType(
-        th.Property("id", th.IntegerType),
+        th.Property("node_id", th.StringType),
+        th.Property("slug", th.StringType),
         th.Property("name", th.StringType),
+        th.Property("description", th.StringType),
     )
 
     answer_object = th.ObjectType(
